2d621a176d25d1dfe6c58a7c86fe91027daf18fc
nomad-mystic <nomadmystics@gmail.com>
2023-10-09 21:35:39 -0700

    kpm: Init commit for sage theme

---
+++ wp-content/cache/acorn/framework/views/3287089ad3a74c014edc7f8086ceadf150d63e75.php
@@ -0,0 +1,2 @@
+<?php (dynamic_sidebar('sidebar-primary')); ?>
+<?php /**PATH /Users/keith/Sites/com.nomadmystic2023/wp-content/themes/nomad-mystic/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
---
+++ wp-content/cache/acorn/framework/views/d78f7aeec09407d82d6a26a273fa07db2786cd7e.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  <?php (dynamic_sidebar('sidebar-footer')); ?>
+</footer>
+<?php /**PATH /Users/keith/Sites/com.nomadmystic2023/wp-content/themes/nomad-mystic/resources/views/sections/footer.blade.php ENDPATH**/ ?>
