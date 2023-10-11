674c366128427b6304ed8e8f6d1326f002349760
nomad-mystic <nomadmystics@gmail.com>
2023-10-11 01:31:56 -0700

    kpm: Continue the process of rebuilding the home animations

--- wp-content/cache/acorn/framework/views/3287089ad3a74c014edc7f8086ceadf150d63e75.php
+++
@@ -1,2 +0,0 @@
-<?php (dynamic_sidebar('sidebar-primary')); ?>
-<?php /**PATH /Users/keith/Sites/com.nomadmystic2023/wp-content/themes/nomad-mystic/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
--- wp-content/themes/nomad-mystic/app/filters.php
+++ wp-content/themes/nomad-mystic/app/filters.php
@@ -14,11 +14,3 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
-
-add_filter('acorn/paths.storage', function ($path) {
-
-    die(var_dump($path));
-
-    return '/testing';
-});
-
