6941f4d1dd2df47314d8d267e9649183a406339c
nomad-mystic <nomadmystics@gmail.com>
2023-10-11 12:45:31 -0700

    kpm: Update composer values

--- wp-content/themes/nomad-mystic/vendor/composer/autoload_static.php
+++ wp-content/themes/nomad-mystic/vendor/composer/autoload_static.php
@@ -296,9 +296,9 @@ class ComposerStaticInitddb6173d2e234b5e5fedb649d315f6b2
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/macroable',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
             1 => __DIR__ . '/..' . '/illuminate/conditionable',
-            2 => __DIR__ . '/..' . '/illuminate/collections',
+            2 => __DIR__ . '/..' . '/illuminate/macroable',
             3 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Session\\' => 
