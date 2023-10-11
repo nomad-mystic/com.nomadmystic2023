1f4815a613925cf2ffe5f4e3bd8d01c2e6cbb915
nomad-mystic <nomadmystics@gmail.com>
2023-10-11 15:28:10 -0700

    kpm: Continue the process of rebuilding the home animations

--- wp-content/themes/nomad-mystic/resources/views/partials/home/built-with.blade.php
+++ wp-content/themes/nomad-mystic/resources/views/partials/home/built-with.blade.php
@@ -1,14 +1,44 @@
-<section class="Home-builtWith"
-         style="background: url(@asset('images/background-built-with.jpg')) no-repeat bottom center; background-size: cover">
+<section class="Home-builtWith flex flex-col items-center py-20"
+         style="background-image: url(@asset('images/background-built-with.jpg'))">
 
     <header class="header">
         <h2>Custom Theme Built with...</h2>
     </header>
 
-    <section class="icons flex">
-        <a href="https://roots.io/" target="_blank" rel="noreferrer">
+    <section class="icons flex justify-between">
+        <a href="https://roots.io/" target="_blank" rel="noreferrer" class="p-4 mx-2">
             <figure class="icon">
                 @svg('si-roots')
+            </figure>
+        </a>
+
+        <a href="https://wordpress.org/" target="_blank" rel="noreferrer" class="p-4 mx-2">
+            <figure class="icon">
+                @svg('si-wordpress')
+            </figure>
+        </a>
+
+        <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer" class="p-4 mx-2">
+            <figure class="icon">
+                @svg('si-tailwindcss')
+            </figure>
+        </a>
+
+        <a href="https://roots.io/" target="_blank" rel="noreferrer" class="p-4 mx-2">
+            <figure class="icon">
+                @svg('si-nodedotjs')
+            </figure>
+        </a>
+
+        <a href="https://roots.io/" target="_blank" rel="noreferrer" class="p-4 mx-2">
+            <figure class="icon">
+                @svg('si-laravel')
+            </figure>
+        </a>
+
+        <a href="https://roots.io/" target="_blank" rel="noreferrer" class="p-4 mx-2">
+            <figure class="icon">
+                @svg('si-vuedotjs')
             </figure>
         </a>
     </section>
