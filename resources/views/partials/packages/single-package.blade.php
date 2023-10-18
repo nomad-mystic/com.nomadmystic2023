fe848aaaf70cd7b315d9ec25af5fe149aab0c87a
nomad-mystic <nomadmystics@gmail.com>
2023-10-18 14:20:55 -0700

    kpm: Continue to build packages component

--- resources/views/partials/packages/single-package.blade.php
+++ resources/views/partials/packages/single-package.blade.php
@@ -1,7 +1,10 @@
 <article>
 
     <div>{{  $package['description'] ?? '' }}</div>
+
+    <!-- https://prismjs.com/ -->
     <div>{{  $package['readme'] ?? '' }}</div>
+
     <div>{{  $package['homepage'] ?? '' }}</div>
     <div>{{  $package['repository']['url'] ?? '' }}</div>
 
