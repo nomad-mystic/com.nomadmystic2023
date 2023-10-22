46673cc3fed4c650ad7a85e015d1e5d1a58992c8
nomad-mystic <nomadmystics@gmail.com>
2023-10-22 09:28:26 -0700

    kpm: Continue to build packages component

--- resources/views/partials/packages/single-package.blade.php
+++ resources/views/partials/packages/single-package.blade.php
@@ -1,29 +1,36 @@
-<section class="SinglePackage w-full">
+<section class="SinglePackage w-full p-6">
     <article>
         <header>
             <h2 class="SinglePackage-title">{{  $package['name'] ?? '' }}</h2>
             <p class="SinglePackage-description">{{  $package['description'] ?? '' }}</p>
         </header>
 
-        <section>
+        <section class="flex items-center">
             <div class="SinglePackage-homepage">
-                {{  $package['homepage'] ?? '' }}
+                <a href="{{ $package['homepage'] ?? '' }}" target="_blank" rel="noreferrer" class="p-4 mx-2">
+                    <figure class="SinglePackage-icon">
+                        @svg('si-github')
+                    </figure>
+                </a>
             </div>
 
             <div class="SinglePackage-created">
-                {{  $package['created'] ?? '' }}
+                <time>
+                    <span>Created: {{ Carbon\Carbon::parse($package['time']['created'] ?? '')->format('F d, Y') }}</span>
+                </time>
             </div>
 
             <div class="SinglePackage-modified">
-                {{  $package['modified'] ?? '' }}
+                <span>Modified: {{ Carbon\Carbon::parse($package['time']['modified'] ?? '')->format('F d, Y') }}</span>
             </div>
         </section>
 
-        <section>
+        <details>
+            <summary></summary>
              <pre>
                 <code class="language-markdown">{{  $package['readme'] ?? '' }}</code>
             </pre>
-        </section>
+        </details>
         <!-- https://prismjs.com/ -->
     </article>
 </section>
