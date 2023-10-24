ad2d6875a0a22de0268ebd12dd6d3f1010ee08a3
nomad-mystic <nomadmystics@gmail.com>
2023-10-24 02:09:23 -0700

    kpm: Continue the process of building the featured section

--- app/Helpers/SchemaHelpers.php
+++ app/Helpers/SchemaHelpers.php
@@ -8,19 +8,28 @@ use Safe\Exceptions\FilesystemException;
 class SchemaHelpers
 {
     /**
-     * @throws FilesystemException
+     * @description Pass this a JSON schema name a it will return the file
+     * @public
+     * @author Keith Murphy | nomadmystics@gmail.com
+     *
+     * @param string $name
+     * @return mixed
      */
-    public static function getSchemaJson(string $name)
+    public static function getSchemaJson(string $name): mixed
     {
         $schema = (object) [];
         $themeDir = get_stylesheet_directory();
         $schemasDir = 'schemas';
 
+        try {
+            if (file_exists("{$themeDir}/{$schemasDir}/{$name}.json")) {
+                $file = \Safe\file_get_contents("{$themeDir}/{$schemasDir}/{$name}.json");
 
-        if (file_exists("{$themeDir}/{$schemasDir}/{$name}.json")) {
-            $file = \Safe\file_get_contents("{$themeDir}/{$schemasDir}/{$name}.json");
-
-            return Utils::jsonDecode($file);
+                return Utils::jsonDecode($file);
+            }
+        } catch (FilesystemException $exception) {
+            $response = $exception->getMessage();
+            echo Utils::jsonEncode($response);
         }
 
         return $schema;
