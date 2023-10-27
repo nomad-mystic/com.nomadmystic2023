b02a0dda08bd0e79430cabef0540719184073e14
nomad-mystic <nomadmystics@gmail.com>
2023-10-26 21:48:00 -0700

    kpm: Continue the process of building the home page

--- resources/scripts/pages/home/home-aspirations-animations.js
+++ resources/scripts/pages/home/home-aspirations-animations.js
@@ -26,36 +26,36 @@ export default class HomeAspirationsAnimations extends AnimationMethods {
      */
     exploreDiscoverCodeAnimations = () => {
         // Animating explore
-        const explorePaths = window.document.getElementsByClassName('homePageExploreDiscoverCodeExplorePath');
+        const explorePaths = window.document.getElementsByClassName('Home-exploreDiscoverCodeExplorePath');
         this.textPathAnimation(explorePaths);
 
         // Animating discover
         setTimeout(() => {
-            const discoverPaths = window.document.getElementsByClassName('homePageExploreDiscoverCodeDiscoverPath');
+            const discoverPaths = window.document.getElementsByClassName('Home-exploreDiscoverCodeDiscoverPath');
             this.textPathAnimation(discoverPaths);
         }, 1000);
 
         // Animating Code
         setTimeout(() => {
-            const codePaths = window.document.getElementsByClassName('homePageExploreDiscoverCodeCodePath');
+            const codePaths = window.document.getElementsByClassName('Home-exploreDiscoverCodeCodePath');
             this.textPathAnimation(codePaths);
         }, 2000);
 
         // after paths stroke-dasharray animations fill the paths with color in left to right order
         setTimeout(() => {
             // fill Explore first
-            const explorePaths = window.document.querySelectorAll('.homePageExploreDiscoverCodeExplorePath');
+            const explorePaths = window.document.querySelectorAll('.Home-exploreDiscoverCodeExplorePath');
             this.fillPathAnimation(explorePaths, '1s');
 
             // fill Discover path after explore finishes
             setTimeout(() => {
-                const discoverPaths = window.document.querySelectorAll('.homePageExploreDiscoverCodeDiscoverPath');
+                const discoverPaths = window.document.querySelectorAll('.Home-exploreDiscoverCodeDiscoverPath');
                 this.fillPathAnimation(discoverPaths, '1s');
 
                 // fill code path after Discover and Explore paths are filled
                 setTimeout(() => {
 
-                    const codePaths = window.document.querySelectorAll('.homePageExploreDiscoverCodeCodePath');
+                    const codePaths = window.document.querySelectorAll('.Home-exploreDiscoverCodeCodePath');
                     this.fillPathAnimation(codePaths, '1s');
 
                 }, 1000); // end Code path fill
--- resources/views/partials/home/dream-not-of-today-svg.blade.php
+++ resources/views/partials/home/dream-not-of-today-svg.blade.php
@@ -1,4 +1,4 @@
-<section class="Home homeAnimationArea flex">
+<section class="Home Home-animationArea flex">
     <figure>
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
              x="0px" y="0px"
