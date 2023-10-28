abefbc482d39184ba0e15536f0f80787e63dbdd0
nomad-mystic <nomadmystics@gmail.com>
2023-10-27 20:39:54 -0700

    kpm: Continue the process of building the home page

--- resources/scripts/pages/home/home-animations.js
+++ resources/scripts/pages/home/home-animations.js
@@ -1,7 +1,11 @@
 import AnimationMethods from '@scripts/pages/home/animation-methods.js';
 import HomeHeaderAnimations from '@scripts/pages/home/home-header-animations.js';
 
-
+/**
+ * @classdesc Starting pace for the homepage animations
+ * @class HomeAnimations
+ * @author Keith Murphy | nomadmystics@gmail.com
+ */
 export default class HomeAnimations {
     constructor()
     {
