814dce4cef7cb3329977fa4fd4dedf03fc31db3c
nomad-mystic <nomadmystics@gmail.com>
2023-10-27 21:22:49 -0700

    kpm: Continue the process of building the home page

--- resources/views/partials/home/portal-svg.blade.php
+++ resources/views/partials/home/portal-svg.blade.php
@@ -1,25 +1,17 @@
-<section class="Home Home-threeFeatures startAnimation flex flex-col md:flex-row">
-    <a class="Home-portals" href="/featured/">
-        <div class="Home-portal">
-            <h4>Featured</h4>
-        </div>
+<section class="Home Home-portals flex flex-col md:flex-row">
+    <a class="Home-portal text-3xl lg:text-4xl" href="/featured/">
+        <h4>Featured</h4>
     </a><!--end featured-->
 
-    <a class="Home-portal" href="/repos/">
-        <div class="Home-portal">
-            <h4>Repositories</h4>
-        </div>
+    <a class="Home-portal text-3xl lg:text-4xl" href="/repos/">
+        <h4>Repositories</h4>
     </a><!--end repos-->
 
-    <a class="Home-portal" href="/packages/">
-        <div class="Home-portal">
-            <h4>Packages</h4>
-        </div>
-    </a><!--end packages-->
+    <a class="Home-portal text-3xl lg:text-4xl" href="/packages/">
+        <h4>Packages</h4>
+    </a>
 
-    <a class="Home-portal" href="/websites/">
-        <div class="Home-portal">
-            <h4>Websites</h4>
-        </div>
-    </a><!--end websites-->
+    <a class="Home-portal text-3xl lg:text-4xl" href="/websites/">
+        <h4>Websites</h4>
+    </a>
 </section><!--Home-threeFeatures-->
